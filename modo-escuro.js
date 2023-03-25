a7b60c3082e1ebf2e3b3778a02b3bae44af5f6a4
Luis Fernando Planella Gonzalez <luis@cyclos.org>
2023-03-25 17:22:40 -0300

    Exemplo de modo escuro

---
+++ modo-escuro.js
@@ -0,0 +1,30 @@
+/** Define o tema atual */
+function defineTema(tema) {
+    if (!tema) {
+        // Primeiro tenta recuperar o que estava salvo
+        tema = localStorage.getItem('tema');
+    }
+    if (!tema) {
+        // Nenhum tema salvo. Pega a preferência
+        const prefereEscuro = window.matchMedia('(preferes-color-scheme: dark)').matches;
+        tema = prefereEscuro ? 'escuro' : 'claro';
+    }
+    // Armazena para a próxima visita
+    localStorage.setItem('tema', tema);
+    // Define os estilos no elemento <body>
+    if (tema === 'claro') {
+        document.body.classList.add('claro');
+        document.body.classList.remove('escuro');
+    } else {
+        document.body.classList.add('escuro');
+        document.body.classList.remove('claro');
+    }
+    document.getElementById('alterna-tema').innerHTML = 
+        document.getElementById(`icone-${tema}`).innerHTML;
+}
+
+/** Alterna o tema atual */
+function alternaTema() {
+    const atual = localStorage.getItem('tema');
+    defineTema(atual == 'claro' ? 'escuro' : 'claro');
+}
